deeb45e148b9ecc74ba836da5d07717fdbea3b67
AfonSir <afonsir17@gmail.com>
2019-10-08 21:23:04 -0300

    Fixing default application route

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    resources "/topics", TopicController
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
